14390eed1f865f76f7aeb20c8ed0387d90fdb231
DimitarYosifov <yosifov1985@gmail.com>
2022-05-14 13:48:22 +0300

    test local port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const cors = require("cors");
-const port = process.env.PORT || 80;
+const port = process.env.port || 80;
 
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors());
